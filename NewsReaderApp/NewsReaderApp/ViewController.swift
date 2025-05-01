f17c4acfaac71e71d47e79ce97a87c841c9faba9
Artiom <artiom.sin@gmail.com>
2025-05-01 13:11:46 +0300

    Initial project setup with UIKit and CoreData enabled

---
+++ NewsReaderApp/NewsReaderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsReaderApp
+//
+//  Created by Artem on 1.05.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
