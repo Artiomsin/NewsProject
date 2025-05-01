b91155ca414bff6c26913c0f2c0b4420a8b4af70
Artiom <artiom.sin@gmail.com>
2025-05-01 14:33:51 +0300

    updating ManageBookmarksUseCase

--- NewsReaderApp/NewsReaderApp/Domain/UseCases/ManageBookmarksUseCase.swift
+++ NewsReaderApp/NewsReaderApp/Domain/UseCases/ManageBookmarksUseCase.swift
@@ -1,1 +1,30 @@
+protocol BookmarkUseCase {
+    func addBookmark(news: News)
+    func removeBookmark(news: News)
+    func getAllBookmarks() -> [News]
+    func isBookmarked(news: News) -> Bool
+}
 
+class ManageBookmarksUseCase: BookmarkUseCase {
+    private let bookmarkStorage: BookmarkStorage
+    
+    init(bookmarkStorage: BookmarkStorage) {
+        self.bookmarkStorage = bookmarkStorage
+    }
+    
+    func addBookmark(news: News) {
+        bookmarkStorage.addBookmark(news: news)
+    }
+    
+    func removeBookmark(news: News) {
+        bookmarkStorage.removeBookmark(news: news)
+    }
+    
+    func getAllBookmarks() -> [News] {
+        return bookmarkStorage.getAllBookmarks()
+    }
+    
+    func isBookmarked(news: News) -> Bool {
+        return bookmarkStorage.isBookmarked(news: news)
+    }
+}
