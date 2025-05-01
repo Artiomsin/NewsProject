c7c69bc24275daa41d3b401e81162dfb1cef0d8f
Artiom <artiom.sin@gmail.com>
2025-05-01 14:12:24 +0300

    Implemented NewsRepository for API fetching and CoreData storage

---
+++ NewsReaderApp/NewsReaderApp/Data/Repository/NewsRepositoryImpl.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class NewsRepositoryImpl: NewsRepository {
+    private let apiService: APIService
+    
+    init(apiService: APIService) {
+        self.apiService = apiService
+    }
+    
+    func fetchNews(query: String, fromDate: String, sortBy: String, completion: @escaping (Result<[News], Error>) -> Void) {
+        apiService.fetchNews(query: query, fromDate: fromDate, sortBy: sortBy, completion: completion)
+    }
+    
+    
+}
