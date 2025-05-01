1697248356f7cf33a278f9bb823fd67dd66f5c9a
Artiom <artiom.sin@gmail.com>
2025-05-01 15:13:07 +0300

    Implemented TabBarController for navigation between News and Bookmarks

--- NewsReaderApp/NewsReaderApp/ViewController.swift
+++ NewsReaderApp/NewsReaderApp/ViewController.swift
@@ -1,19 +1,51 @@
-//
-//  ViewController.swift
-//  NewsReaderApp
-//
-//  Created by Artem on 1.05.25.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+class ViewController: UITabBarController {
+    private let apiService = APIService()
+       private lazy var newsRepository: NewsRepository = NewsRepositoryImpl(apiService: apiService)
+       private lazy var bookmarkStorage: BookmarkStorage = CoreDataBookmarkStorage()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           setupViewControllers()
+           setupTabBarAppearance()
+       }
+       
+       private func setupViewControllers() {
+           // 1. News List
+           let fetchUseCase = FetchNewsUseCase(newsRepository: newsRepository)
+           let bookmarkUseCase = ManageBookmarksUseCase(bookmarkStorage: bookmarkStorage)
+           let newsVM = NewsListViewModel(fetchNewsUseCase: fetchUseCase, bookmarkUseCase: bookmarkUseCase)
+           let newsVC = NewsListViewController(viewModel: newsVM)
+           let newsNav = UINavigationController(rootViewController: newsVC)
+           newsNav.tabBarItem = UITabBarItem(title: "News", image: UIImage(systemName: "newspaper"), tag: 0)
+           
+           // 2. Bookmarks
+           let bookmarksVM = BookmarksViewModel(bookmarkUseCase: bookmarkUseCase)
+           let bookmarksVC = BookmarksViewController(viewModel: bookmarksVM)
+           let bookmarksNav = UINavigationController(rootViewController: bookmarksVC)
+           bookmarksNav.tabBarItem = UITabBarItem(title: "Bookmarks", image: UIImage(systemName: "bookmark"), tag: 1)
+           
+           viewControllers = [newsNav, bookmarksNav]
+       }
+       
+       private func setupTabBarAppearance() {
+                   let navBarAppearance = UINavigationBarAppearance()
+                   navBarAppearance.configureWithOpaqueBackground()
+                   navBarAppearance.backgroundColor = .systemBackground
+                   navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.label]
+                   
+                   UINavigationBar.appearance().standardAppearance = navBarAppearance
+                   UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
+                   UINavigationBar.appearance().tintColor = .systemBlue
+           
+                   let tabBarAppearance = UITabBarAppearance()
+                   tabBarAppearance.configureWithOpaqueBackground()
+                   tabBarAppearance.backgroundColor = .systemBackground
+                   
+                   UITabBar.appearance().standardAppearance = tabBarAppearance
+                   UITabBar.appearance().tintColor = .systemBlue
+       }
 }
+
 
